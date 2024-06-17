08f9d9cc8e2b3dfb509cf24fc3a3052ed1125b98
AatirNadim <aatir.nadim@gmail.com>
2024-06-17 18:08:47 +0530

    feat: fundamental packages

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { INestApplication, ValidationPipe } from "@nestjs/common";
+// import { DatabaseExceptionFilter } from "./filters/database-exception.filter";
+import * as cookieParser from "cookie-parser";
+import { ConfigService } from "@nestjs/config";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app: INestApplication = await NestFactory.create(AppModule, {
+    cors: true,
+    logger: ["debug", "error", "log", "verbose", "warn"],
+  });
+
+  app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe());
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>("PORT") || 3001;
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle("Movie-Backend API")
+    .setDescription("The Movie-Backend API description")
+    .setVersion("1.0")
+    .build();
+
+  const swaggerDoc = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup("api-docs", app, swaggerDoc);
+
+  await app.listen(port);
 }
 bootstrap();
